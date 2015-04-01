c1eda9942c11a9bde275f917708b01c8e249c851
ron8mcr <rons_mail@mail.ru>
2015-04-01 13:56:11 +0700

    day2

--- post_app/views.py
+++ post_app/views.py
@@ -1,3 +1,4 @@
+#coding: UTF-8
 from django.views.generic import ListView, TemplateView
 from post_app.models import Slider, News
 
@@ -8,8 +9,15 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['slider'] = Slider.objects.all()
+
         context['news'] = News.objects.all()[:1]
         context['news_offset'] = 1
+        context['news_list_limit'] = 1
+        context['news_url'] = 'news'
+        context['news_more_button_id'] = 'news-more-id'
+        context['news_more_button_label'] = u'Ещё новости'
+        context['news_tab_id'] = 'first'
+
         return context
 
 
@@ -29,7 +37,12 @@ class PaginationMixin(object):
 
     def get_context_data(self, **kwargs):
         context = super(PaginationMixin, self).get_context_data(**kwargs)
-        context['object_list_offset'] = self.default_list_limit
+        context['default_list_limit'] = self.default_list_limit
+        context['data_url'] = self.data_url
+        context['more_button_id'] = self.more_button_id
+        context['more_button_label'] = self.more_button_label
+        context['tab_id'] = 'first'
+
         if self.offset and self.limit:
             context['object_list_offset'] = self.offset + self.limit
         else:
@@ -43,3 +56,6 @@ class NewsView(PaginationMixin, ListView):
     model = News
     data_url = 'news'
     default_list_limit = 1
+    more_button_id = 'news-more-id'
+    more_button_label = u'Ещё новости'
+    tab_id = 'First'
